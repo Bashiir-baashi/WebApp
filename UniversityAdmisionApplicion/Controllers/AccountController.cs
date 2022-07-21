8b60b158c3da65e19e460653b2cd0bcf091f6123
Bashiir-baashi <bashiirabdi991@gmail.com>
2022-07-21 11:06:49 +0300

    first commit

---
+++ UniversityAdmisionApplicion/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityAdmisionApplicion.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Sing()
+        {
+            return View();
+        }public IActionResult forget()
+        {
+            return View();
+        }
+    }
+}
---
+++ UniversityAdmisionApplicion/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityAdmisionApplicion.Controllers
+{
+    public class DashboardController : Controller
+    {
+        
+        public IActionResult profile()
+        {
+            return View();
+        }
+     public IActionResult Applications()
+        {
+            return View();
+        }
+    public IActionResult Process()
+        {
+            return View();
+        }
+    }
+}
